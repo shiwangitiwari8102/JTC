37abce899bcaaa0abb0eb40ea6ce2bf2c1512f9c
shiwangitiwari8102 <102220186+shiwangitiwari8102@users.noreply.github.com>
2022-10-29 20:36:35 +0530

    Add files via upload

---
+++ arr2.js
@@ -0,0 +1,21 @@
+
+let coffeeMenu =  {
+      espresso: {
+        milk: 60, 
+        cream: 75, 
+        latte: 100
+        },
+      cappuccino: {
+        milk: 80, 
+        cream: 90, 
+        latte: 125 
+        },
+      latte: {
+        milk: 100, 
+        cream: 125, 
+        latte: 150
+        }
+    }
+
+sss
+   
